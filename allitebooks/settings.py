08e845d3bbfe553ec6ac4d07a5a570cdd55d4423
Alexander Swensen <alex.swensen@gmail.com>
2018-02-11 14:10:21 -0600

    robots.txt is ok

--- allitebooks/settings.py
+++ allitebooks/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'allitebooks.spiders'
 USER_AGENT = 'allitebooks (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
