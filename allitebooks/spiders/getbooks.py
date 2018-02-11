9e54c13b43be48efa36b5642d40ed67caa19da19
Alexander Swensen <alex.swensen@gmail.com>
2018-02-11 12:39:20 -0600

    wip

--- allitebooks/spiders/getbooks.py
+++ allitebooks/spiders/getbooks.py
@@ -5,11 +5,14 @@ import scrapy
 class BookspiderSpider(scrapy.Spider):
     name = 'getbooks'
     allowed_domains = ['www.allitebooks.com']
-    start_urls = ['http://www.allitebooks.com']
+    # start_urls = ['http://www.allitebooks.com']
     def start_requests(self):
-        urls = [
-            'http://www.allitebooks.com/',
-        ]
+        urls = ['http://www.allitebooks.com/']
+
+        for i in range(2, 747):
+            urls.append(f'http://www.allitebooks.com/page/{i}/')
+
+        print(urls)
 
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
