d89fbbaa934666883102ea8769604e8749a0fe06
Alexander Swensen <alex.swensen@gmail.com>
2018-02-11 11:48:12 -0600

    place to start

---
+++ allitebooks/spiders/getbooks.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class BookspiderSpider(scrapy.Spider):
+    name = 'getbooks'
+    allowed_domains = ['www.allitebooks.com']
+    start_urls = ['http://www.allitebooks.com']
+    def start_requests(self):
+        urls = [
+            'http://www.allitebooks.com/',
+        ]
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'books-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
