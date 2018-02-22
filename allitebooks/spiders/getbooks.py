203a40778dd39823eefe337d89a10e353fe97aa0
Shemaiah Telemaque <drmaq63@gmail.com>
2018-02-21 20:57:29 -0500

    Using in house downloader

--- allitebooks/spiders/getbooks.py
+++ allitebooks/spiders/getbooks.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import requests
+from requests import codes, get
 from clint.textui import progress
+from scrapy.loader import ItemLoader
+from allitebooks.items import AllitebooksItem
 
 class BookspiderSpider(scrapy.Spider):
     name = 'getbooks'
     allowed_domains = ['www.allitebooks.com']
+
     # start_urls = ['http://www.allitebooks.com']
     def start_requests(self):
         urls = ['http://www.allitebooks.com/']
 
-        for i in range(2, 747):
+        for i in range(1, 747):
             urls.append(f'http://www.allitebooks.com/page/{i}/')
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
-
 
     def parse(self, response):
 
@@ -25,8 +27,9 @@ class BookspiderSpider(scrapy.Spider):
             book_title = book_body.css('.entry-title a::text').extract_first()
             yield response.follow(book_link, callback=self.parse_book)
 
-
     def parse_book(self, response):
+        l = ItemLoader(item=AllitebooksItem(),response=response)
+
         title_body = response.css('header.entry-header')
         title = title_body.css('h1::text').extract_first()
         subtitle = title_body.css('h4::text').extract_first()
@@ -35,32 +38,36 @@ class BookspiderSpider(scrapy.Spider):
         book_detail = metadata_entry.css('.book-detail')
         footer = response.css('footer.entry-footer')
         download_link = footer.css('.download-links a::attr(href)').extract_first()
+        print("link" ,download_link)
 
-        self.download_file(download_link)
+        l.add_value('title',title)
+        l.add_value('download_link', download_link)
+        #l.add_value('')
 
+        return l.load_item()
+        #self.download_file(download_link)
 
-    def download_file(self, url):
-        local_filename = url.split('/')[-1]
+    #def download_file(self, url):
+        #local_filename = url.split('/')[-1]
 
-        pdf = requests.get(url, stream=True)
-        pdf.status_code
-        if pdf.status_code == requests.codes.ok:
-            print("Downloading file", local_filename)
-            #self.log('Downloading filename:', local_filename)
-        else:
-            self.log("!!!Filed to download.!!!")
-        #self.log('Downloading filename:', local_filename)
-        self.log('Status Code:', pdf.status_code)
-        #print('Start the download of %s: local_filename')
-
-        with open(f'./books/{local_filename}', 'wb') as fd:
-            total_size = int(pdf.headers.get('content-length'))
-            print(total_size)
-            for chunk in progress.bar(pdf.iter_content(chunk_size=1024),expected_size=(total_size/1024)+1):
-                if chunk:
-                    fd.write(chunk)
-                    fd.flush()
-            #for chunk in pdf.iter_content(1024):
-                #fd.write(chunk)
-                #self.log('writing chunk ')
-
+       #pdf = get(url, stream=True)
+        # status_code = pdf.status_code
+        #if pdf.status_code == codes.ok:
+            #print("Downloading file:", local_filename)
+            # self.log('Downloading filename:', local_filename)
+            #with open(f'./books/{local_filename}', 'wb') as fd:
+                #total_size = int(pdf.headers.get('content-length'))
+                #print(total_size)
+                #for chunk in progress.bar(pdf.iter_content(chunk_size=1024), expected_size=(total_size / 1024) + 1):
+                    #if chunk:
+                       # fd.write(chunk)
+                        #fd.flush()
+                # for chunk in pdf.iter_content(1024):
+                # fd.write(chunk)
+                #fd.flush
+                # self.log('writing chunk ')
+       # else:
+            #self.log("!!!Filed to download.!!!")
+        # self.log('Downloading filename:', local_filename)
+        #self.log('Status Code:', pdf.status_code)
+        # print('Start the download of %s: local_filename')
